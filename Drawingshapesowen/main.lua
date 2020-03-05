e0fdffd49e74193e0d17e353636144f5a2dcdfed
Owen Meagher <owen.meagher@ocsbstudent.ca>
2020-03-04 19:12:53 -0500

    yh

--- Drawingshapesowen/main.lua
+++ Drawingshapesowen/main.lua
@@ -2,14 +2,55 @@
 --Name:Owen
 --Course:ISC20
 --This program displays four different polygons.
+<<<<<<< HEAD
+---------------------------------------------------
+
+--hide the Status Bar
+display.setStatusBar(display.HiddenStatusBar)
+
+local rect =display.newRect (160, 240, 150, 50)
+
+local display.newtriangle (350, 420, 340, 240)
+
+local display.newPentagon ( 120, 200, 110, 10)
+
+local dispaly.newhexagon (640, 700, 610, 520)
+
+
+
+
+=======
+------------------------------------------------------------------------
+display.setStatusBar(display.HiddenStatusBar)
+
 
 local myTriangle
-local myTrapezoid
-local myHexagon
-local my --------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+local triangleText = display.newText
+local triangleVertices = {0,-50, -50,50, 50,50}
+local triangleX = 130
+local triangleY = 100
 
--- Your code here
+
+myTriangle = display.newPolygon (triangleX, triangleY, triangleVertices )
+--------------------------------------------------------------------------------------
+
+local myTrapezoid
+local trapezoidText
+local trapezoidVertices = {-50,-50, -100,50, 100,50, 50,-50}
+local trapezoidX = 500
+local trapezoidY = 500
+
+myTrapezoid = display.newPolygon (trapezoidX, trapezoidY, trapezoidVertices )
+---------------------------------------------------------------------------------------
+
+
+local myHexagon
+local myPentagon
+local myHeptagon
+
+
+
+
+
+>>>>>>> 0cc20161c77bdac2fc60b64335e2426e7adc1170
+
--- TouchDragOwen/main.lua
+++ TouchDragOwen/main.lua
@@ -1,7 +1,55 @@
 -----------------------------------------------------------------------------------------
---
--- main.lua
---
+--Title: Touch and Drag
+--Name: Owen
+--Course: ICS20
+--This program displays images that react to a mouse or a persons finger.
 -----------------------------------------------------------------------------------------
 
--- Your code here
+-- Hide the status bar
+display.setStatusBar(display.hiddenStatusBar)
+
+--local variables
+local backgroundImage = display.newImageRect("Images/background.png", 2048, 1536)
+local beetleShip = display.newImageRect("Images/beetleshipcopy.png", 150, 150)
+local beetleShipWidth =beetleShip.width
+local beetleShipHieght = beetleShip.Hieght
+
+local rocketShip = display.newImageRect("Images/rocketShip.png", 150, 150)
+local rocketShipWidth = rocketShip.Width
+local rocketshipHieght = rocketship.Hieght
+
+--my boolean variables to keep track of which object i touched first 
+local alreadyTouchedBeetleShip = false 
+local alreadyTouchedRocketShip = false
+
+--set the initial x and y position of my image 
+beetleShip.x = 400
+beetleShip.y = 500
+
+rocketShip.x = 300
+rocketship.y = 200
+
+-- Function: Rocketshiplistener
+-- Input: touch listener
+-- Output: none
+--Description: when rockethip is touched, move it
+local function Rocketshiplistener(touch)
+
+	if (touch.phase == "began") then 
+		if (alreadyTouchedRocketShip == false) then
+			alreadyTouchedBeetleShip = true 
+		end 
+	end
+
+	if ( (touch.phase == "moved") and (alreadyTouchedBeetleShip == true) ) then
+		beetleShip.x = touch.x
+		beetleShip.y = touch.y 
+	een
+	if (touch.phase == "ended") then
+		alreadyTouchedBeetleShip = false
+		alreadyTouchedRocketShip = false 
+	end 
+end 
+
+--add the respective listeners to each object 
+rocketShip:addEventListener("touch", Rocketshiplistener)
