8bca049c537a90f66cbbe0094c6e6d88620ed659
Owen Meagher <owen.meagher@ocsbstudent.ca>
2020-03-06 11:49:34 -0500

    gfb

--- Drawingshapesowen/main.lua
+++ Drawingshapesowen/main.lua
@@ -9,74 +9,69 @@
 --hide the Status Bar
 display.setStatusBar(display.HiddenStatusBar)
 
-set.fillColour(0.1, 0.4, 0.6)
 
-set.strokeColour(0.9, 0.7, 0.6)
-
-local rect =display.newRect (160, 240, 150, 50)
-
-local display.newtriangle (350, 420, 340, 240)
-
-local display.newPentagon ( 120, 200, 110, 10)
-
-local dispaly.newhexagon (640, 700, 610, 520)
-------------------------------------------------------------------------
+-------------------------------------------------------
 
 
-set.fillColour(0.7, 0.5, 0.1)
-
-set.srtokeColour(0.4, 0.6, 0.3)
-
-local myTriangle
-local triangleText = display.newText("Triangele")
 local triangleVertices = {0,-50, -50,50, 50,50}
 local triangleX = 130
 local triangleY = 100
 
+local myTriangle = display.newPolygon(triangleX, triangleY, triangleVertices )
+local triangleText = display.newText("Triangle", 115, 120, nil, 50 )
 
-myTriangle = display.newPolygon (triangleX, triangleY, triangleVertices )
+myTriangle:setFillColor(0.7, 0.5, 0.1)
+
+myTriangle:setStrokeColor(0.4, 0.6, 0.3)
+
+
+
 --------------------------------------------------------------------------------------
 
-set.fillcolour(0.1, 0.3, 0.8,)
-
-set.strokeColour(0.5, 0.2, 0.3)
 
 
-local myTrapezoid
-local trapezoidText = display.newText("Trapezoid")
 local trapezoidVertices = {-50,-50, -100,50, 100,50, 50,-50}
 local trapezoidX = 500
 local trapezoidY = 500
+local myTrapezoid = display.newPolygon (trapezoidX, trapezoidY, trapezoidVertices )
+local trapezoidText = display.newText("Trapezoid", 485,490, nil, 50)
 
-myTrapezoid = display.newPolygon (trapezoidX, trapezoidY, trapezoidVertices )
+myTrapezoid:setFillColor(0.1, 0.3, 0.8)
+
+myTrapezoid:setStrokeColor(0.5, 0.2, 0.3)
+
 ---------------------------------------------------------------------------------------
 
-set.fillColour(0.9, 0.3, 0.5)
-
-set.strokeColour(0.1, 0.6, 0.3)
-
-
-local myHexagon
-local hexagonText = display.newText("Hexagon")
-local hexagonVertices {-600,-600, -650,600, 650,600, 600,-600, -550,550, 700,650}
+local hexagonVertices = {-100,-100, -150,100, 150,100, 100,-100, -150,150, 100,150}
 local hexagonX = 700
 local hexagonY = 700
 
-myHexagon = display.newPolygon (hexagonX, hexagonY, hexagonVertices )
+
+local myHexagon = display.newPolygon (hexagonX, hexagonY, hexagonVertices )
+local hexagonText = display.newText("Hexagon", 685,690, nil, 50)
+
+
+myHexagon:setFillColor(0.9, 0.3, 0.5)
+
+myHexagon:setStrokeColor(0.1, 0.6, 0.3)
+
+
 ----------------------------------------------------------------------------------------
 
-set.fillColour(0.5, 0.6, 0.4,)
 
-set.strokeColour(0.1, 0.4, 0.6)
-
-
-local myPentagon
-local pentagonText = display.newText("Pentagon")
-local pentagonVertices {-300,-300, -350,300, 350,300, 300,-300, -250,250}
+local pentagonVertices = {-300,-300, -350,300, 350,300, 300,-300, -250,250}
 local pentagonX = 300
 local pentagonY = 300
 
-myPentagon = display.newPolygon (pentagonX, pentagonY, pentagonVertices )
+local myPentagon  = display.newPolygon (pentagonX, pentagonY, pentagonVertices )
+local pentagonText = display.newText("Pentagon", 285, 290, nil, 50)
+
+
+
+set.fillColour(0.5, 0.6, 0.4)
+
+set.strokeColour(0.1, 0.4, 0.6)
+
 ------------------------------------------------------------------------------------------
 
 
@@ -87,7 +82,7 @@ strokeColour(0.2, 0.1, 0.5)
 
 local myHeptagon
 local heptagonText = display.newText("Heptagon")
-local heptagonVertices {-900,-900, -950,900, 950,900, 900,-900, -850,850, }
+local heptagonVertices = {-900,-900, -950,900, 950,900, 900,-900, -850,850, }
 local heptagonX = 900
 local heptagonY = 870
 
@@ -101,7 +96,7 @@ set.strokeColour(0.3, 0.4, 0.6)
 
 local myRectangle
 local my RectangleText = display.newText("Rectangle")
-local rectangleVertices {-700,-700, -650, 600, 650,700, 700,-700}
+local rectangleVertices = {-700,-700, -650, 600, 650,700, 700,-700}
 local rectengleX = 700
 local rectangleY = 670
 
--- TouchDragOwen/main.lua
+++ TouchDragOwen/main.lua
@@ -10,46 +10,73 @@ display.setStatusBar(display.hiddenStatusBar)
 
 --local variables
 local backgroundImage = display.newImageRect("Images/background.png", 2048, 1536)
-local beetleShip = display.newImageRect("Images/beetleshipcopy.png", 150, 150)
-local beetleShipWidth =beetleShip.width
-local beetleShipHieght = beetleShip.Hieght
+local beetleship = display.newImageRect("Images/beetleship.png", 150, 150)
+local beetleshipWidth = beetleship.width
+local beetleshipHeight = beetleship.height
 
-local rocketShip = display.newImageRect("Images/rocketShip.png", 150, 150)
-local rocketShipWidth = rocketShip.Width
+local rocketship = display.newImageRect("Images/rocketship.png", 150, 150)
+local rocketshipWidth = rocketship.Width
 local rocketshipHieght = rocketship.Hieght
 
 --my boolean variables to keep track of which object i touched first 
-local alreadyTouchedBeetleShip = false 
-local alreadyTouchedRocketShip = false
+local alreadyTouchedBeetleship = false 
+local alreadyTouchedRocketship = false
 
 --set the initial x and y position of my image 
-beetleShip.x = 400
-beetleShip.y = 500
+beetleship.x = 400
+beetleship.y = 500
 
-rocketShip.x = 300
+rocketship.x = 300
 rocketship.y = 200
 
--- Function: Rocketshiplistener
+-- Function: BeetleshipListener
 -- Input: touch listener
 -- Output: none
---Description: when rockethip is touched, move it
-local function Rocketshiplistener(touch)
+--Description: when beetleship is touched, move it
+local function BeetleshipListener(touch)
 
 	if (touch.phase == "began") then 
-		if (alreadyTouchedRocketShip == false) then
-			alreadyTouchedBeetleShip = true 
+		if (alreadyTouchedRocketship == false) then
+			alreadyTouchedBeetleship = true 
 		end 
 	end
 
-	if ( (touch.phase == "moved") and (alreadyTouchedBeetleShip == true) ) then
-		beetleShip.x = touch.x
-		beetleShip.y = touch.y 
-	een
+	if ( (touch.phase == "moved") and (alreadyTouchedBeetleship == true) ) then
+		beetleship.x = touch.x
+		beetleship.y = touch.y 
+	end
+
 	if (touch.phase == "ended") then
-		alreadyTouchedBeetleShip = false
-		alreadyTouchedRocketShip = false 
+		alreadyTouchedBeetleship = false
+		alreadyTouchedBeetleship = false 
 	end 
 end 
 
 --add the respective listeners to each object 
-rocketShip:addEventListener("touch", Rocketshiplistener)
+beetleship:addEventListener("touch", BeetleshipListener)
+
+-- Function: RocketshipListener
+-- Input: touch listener
+-- Output: none
+--Description: when rockethip is touched, move it
+local function RocketshipListener(touch)
+
+	if (touch.phase == "began") then 
+		if (alreadyTouchedBeetleship == false) then
+			alreadyTouchedRocketship = true 
+		end 
+	end
+
+	if ( (touch.phase == "moved") and (alreadyTouchedRocketship == true) ) then
+		rocketship.x = touch.x
+		rocketship.y = touch.y 
+	end
+
+	if (touch.phase == "ended") then
+		alreadyTouchedRocketship = false
+		alreadyTouchedRocketship = false 
+	end 
+end 
+
+--add the respective listeners to each object 
+rocketship:addEventListener("touch", RocketshipListener)
